e36be8793500397046e0cdc65aa73bc40eb94b44
unaruz <unaruz@gmail.com>
2018-09-16 00:10:39 +0200

    test de la classe

--- Chapitre2_Cnam/src/ChapitreDeuxCnam.java
+++ Chapitre2_Cnam/src/ChapitreDeuxCnam.java
@@ -4,6 +4,13 @@ public class ChapitreDeuxCnam {
 	public static void main(String[] args) {
 		Compte c1 = new Compte();
 		Compte c2 = new Compte();
+		
+		c1 = c2;
+		c1.solde = 100;
+		c1.deposer(300);
+		System.out.println("solde c1 = " + c1.solde);
+		System.out.println("solde c2 = " + c2.solde);
+		
 	}
 
 }
